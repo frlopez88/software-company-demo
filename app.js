cf0fcbce4bbc815d970ff2bb651e739f07ee3a38
Fernando Lopez <frlopez88@icloud.com>
2025-12-08 19:20:57 -0600

    Reading data from the table of products

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 let url = "https://bdygoubdkehbckhaomol.supabase.co/rest/v1/leads";
 let apikey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJkeWdvdWJka2VoYmNraGFvbW9sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4MDY3MzYsImV4cCI6MjA4MDM4MjczNn0.z0e3pKF4YvDuP3vHBJEvd8BWQfkuB4NJsEGjrlfB4JQ";
+let urlProducts = "https://bdygoubdkehbckhaomol.supabase.co/rest/v1/products";
+
 
 async function createLead(){
 
@@ -25,5 +27,50 @@ async function createLead(){
     })
 
     console.log("Lead Created")
+
+}
+
+
+async function getProducts(){
+
+    // promise= is an operation JS loading in the background 
+    let response = await fetch(urlProducts , {
+        method: "GET",
+        headers: {
+            "apikey" : apikey,
+            "Content-Type" : "application/json"
+        }
+    });
+
+    let data = await response.json();
+
+    let cardsContainer = document.getElementById("cardsContainer");
+
+    for(let i = 0; i < data.length; i++){
+
+        cardsContainer.innerHTML += `
+        
+        <div class="col-md-6 col-lg-3 mb-3">
+          <div class="card" style="width: 18rem">
+            <img
+              src="${data[i].image_url}"
+              class="card-img-top"
+              alt="${data[i].name}"
+            />
+            <div class="card-body">
+              <h5 class="card-title">${data[i].name}</h5>
+              <p class="card-text">
+                ${data[i].description}
+              </p>
+              <p class="btn btn-primary">$ ${data[i].price}</p>
+            </div>
+          </div>
+        </div>
+
+        `
+
+    }
+
+
 
 }
